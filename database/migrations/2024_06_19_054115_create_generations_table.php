d978aaffbc9508626653b06ac466e3a85620dbe4
manyopensource <29418829+manyopensource@users.noreply.github.com>
2024-06-19 18:33:58 +0300

    Create models and migrations for car models and its generations

---
+++ database/migrations/2024_06_19_054115_create_generations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('generations', function (Blueprint $table) {
+            $table->id();
+            $table->string('market');
+            $table->string('name');
+            $table->string('period');
+            $table->string('gen');
+            $table->string('image');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('generations');
+    }
+};
